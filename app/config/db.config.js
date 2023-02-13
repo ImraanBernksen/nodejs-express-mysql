f384cac3e020c9c83c1441fc39b0885c165d78f0
Imraan <imraanbernksen.com>
2023-02-13 12:20:54 +0000

    first commit

---
+++ app/config/db.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    HOST: "b9jjvsst6dmjeguy8g5b-mysql.services.clever-cloud.com",
+    USER: "uufg3nljr2fc9ddi",
+    PASSWORD: "ToXIf4V9FXzmA943Z48d",
+    DB: "b9jjvsst6dmjeguy8g5b",
+    dialect: "mysql",
+    pool: {
+      max: 15,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    }
+  };
